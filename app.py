8e05dd596c066486e9acd01ab170215391891f99
Andrew Szucs <andrew0szucs@gmail.com>
2017-03-04 00:16:20 +0100

    feat(endpoints): add /cluster and /sentiment endpoints

--- app.py
+++ app.py
@@ -1,5 +1,36 @@
 from predict_conversation_cluster import predict_cluster
 from predict_message_sentiment import predict_sentiment
 
-print(predict_cluster('Fuck this shit'))
-print(predict_sentiment('Fuck this shit'))
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+@app.route('/cluster', methods=['POST'])
+def get_cluster():
+
+    request_data = request.get_json()
+
+    cluster = predict_cluster(request_data['text'])
+    print(cluster)
+    response = {
+        'clusterId': cluster[0],
+        'label': cluster[1]
+    }
+
+    return jsonify(response)
+
+@app.route('/sentiment', methods=['POST'])
+def get_sentiment():
+
+    request_data = request.get_json()
+
+    sentiment = predict_sentiment(request_data['text'])
+    response = {
+        'sentiment': sentiment
+    }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()
+    
